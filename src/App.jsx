ff8be24e0b30fca1ef84eac9a0ae46fb21f08ad8
JustKooba <jakubsakw2009@gmail.com>
2023-08-17 21:46:51 +0200

    implemented evaluation logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 function App() {
   const [calc, setCalc] = useState("");
-  const [result, setResuly] = useState("");
+  const [result, setResult] = useState("");
 
   const operators = ["/", ".", "+", "*", "-"];
 
@@ -22,6 +22,47 @@ function App() {
     if (calc.length === 0) return;
     setCalc(calc.slice(0, -1));
   };
+
+  const calcResult = () => {
+    try {
+      if (calc === "") return;
+
+      const parts = calc.split(/([+\-*/])/);
+
+      let currentValue = 0;
+      let currentOperator = "+";
+
+      for (let part of parts) {
+        if (operators.includes(part)) {
+          currentOperator = part;
+        } else {
+          const num = parseFloat(part);
+          if (!isNaN(num)) {
+            switch (currentOperator) {
+              case "+":
+                currentValue += num;
+                break;
+              case "-":
+                currentValue -= num;
+                break;
+              case "*":
+                currentValue *= num;
+                break;
+              case "/":
+                currentValue /= num;
+                break;
+              default:
+                break;
+            }
+          }
+        }
+      }
+
+      setResult(currentValue.toString());
+    } catch (error) {
+      setResult("Error");
+    }
+  };
   return (
     <div className="container">
       <div className="display">
@@ -32,53 +73,58 @@ function App() {
         </h3>
       </div>
       <div className="buttons-wrapper">
-        <button className="button" onClick={() => updateCalc("7")}>
-          7
+        <button className="button" onClick={() => updateCalc("9")}>
+          9
         </button>
         <button className="button" onClick={() => updateCalc("8")}>
           8
         </button>
-        <button className="button" onClick={() => updateCalc("9")}>
-          9
-        </button>
-        <button className="button action-btn" onClick={() => updateCalc("*")}>
-          *
-        </button>
-        <button className="button" onClick={() => updateCalc("4")}>
-          4
-        </button>
-        <button className="button" onClick={() => updateCalc("5")}>
-          5
+        <button className="button" onClick={() => updateCalc("7")}>
+          7
         </button>
         <button className="button" onClick={() => updateCalc("6")}>
           6
         </button>
-        <button className="button action-btn" onClick={() => updateCalc("/")}>
-          /
+        <button className="button" onClick={() => updateCalc("5")}>
+          5
         </button>
-        <button className="button" onClick={() => updateCalc("1")}>
-          1
-        </button>
-        <button className="button" onClick={() => updateCalc("2")}>
-          2
+        <button className="button" onClick={() => updateCalc("4")}>
+          4
         </button>
         <button className="button" onClick={() => updateCalc("3")}>
           3
         </button>
-        <button className="button action-btn" onClick={() => updateCalc("-")}>
-          -
+        <button className="button" onClick={() => updateCalc("2")}>
+          2
+        </button>
+        <button className="button" onClick={() => updateCalc("1")}>
+          1
         </button>
         <button className="button" onClick={() => updateCalc("0")}>
           0
         </button>
-        <button className="button action-btn" onClick={() => updateCalc(".")}>
-          .
-        </button>
         <button className="button action-btn" onClick={() => del()}>
           DEL
         </button>
+        <button className="button action-btn" onClick={calcResult}>
+          =
+        </button>
+      </div>
+      <div className="action-buttons">
+        <button className="button action-btn" onClick={() => updateCalc(".")}>
+          .
+        </button>
+        <button className="button action-btn" onClick={() => updateCalc("-")}>
+          -
+        </button>
         <button className="button action-btn" onClick={() => updateCalc("+")}>
           +
+        </button>
+        <button className="button action-btn" onClick={() => updateCalc("/")}>
+          /
+        </button>
+        <button className="button action-btn" onClick={() => updateCalc("*")}>
+          *
         </button>
       </div>
     </div>
